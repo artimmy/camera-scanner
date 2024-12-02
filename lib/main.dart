af2e64d9a4fe2da35a3f736dcb89f38782438dbf
artimmy <arthurtimmy@hotmail.com>
2024-12-02 01:19:22 -0300

    app icon

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Document Scanner',
+      title: 'Camera Scanner',
       theme: ThemeData(primarySwatch: Colors.blue),
       home: const HomeScreen(),
     );
